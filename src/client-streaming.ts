6bfa846208d0da3354b3e720f7963b0986d85729
Josh Adams <josh@dbadbadba.com>
2025-12-26 17:21:44 -0600

    A few fixes from testing

--- src/client-streaming.ts
+++ src/client-streaming.ts
@@ -6,9 +6,15 @@ export class ServerInputStream implements GrpcServerInputStream {
   // eslint-disable-next-line prettier/prettier
   constructor(private callId: number, private clientId: number, private method: string) { }
   send(data: Uint8Array): Promise<void> {
-    return Grpc.clientStreamingCall(this.callId, this.clientId, this.method, {
-      data: fromByteArray(data),
-    }, {});
+    return Grpc.clientStreamingCall(
+      this.callId,
+      this.clientId,
+      this.method,
+      {
+        data: fromByteArray(data),
+      },
+      {}
+    );
   }
   complete(): Promise<void> {
     return Grpc.finishClientStreaming(this.callId);
